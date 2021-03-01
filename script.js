4a8e2d26e1e4455fabe76d60aebda800f9b588b0
Safia Ali <safiaali@hotmail.com>
2021-03-01 16:10:18 +0000

    Add const variables for buttons, timer, input.

---
+++ script.js
@@ -0,0 +1,30 @@
+const startBtn = ;
+const resetBtn = ;
+const pauseBtn= ;
+
+const intervalTimer= ;
+const roundCounter= ;
+const shortBreakTimer=;
+const longBreakTimer=;
+
+const setIntervalTime=;
+const setShortBreakTime=;
+const setLongBreakTime=;
+
+// start countdown timer
+function startInterval(){
+
+}
+
+// stop countdown timer
+function stopInterval(){
+
+}
+
+// event listener for start button 
+
+
+// event listener for pause button 
+
+
+// event listener for reset button 
